250e10fd6afdb6b770a8242f179440da49b2c035
J4mStuff <59264088+J4mStuff@users.noreply.github.com>
2026-02-10 00:12:16 +0100

    update from laptop

---
+++ link_laptop.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/zsh
+
+ln -s dot-config/hypr/overrides/laptop.conf dot-config/hypr/overrides/overides.conf
+ln -s dot-config/ghostty/laptop_config dot-config/ghostty/config
+ln -s dot-config/fuzzel/laptop.ini dot-config/fuzzel/fuzzel.ini
---
+++ link_pc.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/zsh
+
+ln -s dot-config/hypr/overrides/pc.conf dot-config/hypr/overrides/overides.conf
+ln -s dot-config/ghostty/pc_config dot-config/ghostty/config
+ln -s dot-config/fuzzel/pc.ini dot-config/fuzzel/fuzzel.ini
