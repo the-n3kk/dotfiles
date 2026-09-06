923ae4a388fb7a77cae3154705333577335e220f
J4mStuff <59264088+J4mStuff@users.noreply.github.com>
2026-09-06 23:38:09 +0200

    update noctalia

--- dot-config/hypr/configs/window_rules.lua
+++ dot-config/hypr/configs/window_rules.lua
@@ -22,6 +22,17 @@ hl.layer_rule({
 	match = { namespace = "waybar" },
 })
 
+hl.layer_rule({
+	name = "noctalia",
+	match = {
+		namespace = "^noctalia-(bar-.+|notification|dock|panel|attached-panel|osd|window-switcher)$",
+	},
+	no_anim = true,
+	ignore_alpha = 0.5,
+	blur = true,
+	blur_popups = true,
+})
+
 hl.window_rule({
 	-- Fix some dragging issues with XWayland
 	name = "fix-xwayland-drags",
