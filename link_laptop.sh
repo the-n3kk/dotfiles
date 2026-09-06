70be76fcf6277f1d8525129a524c3c474a809972
J4mStuff <59264088+J4mStuff@users.noreply.github.com>
2026-09-06 19:45:29 +0200

    cleanup

--- link_laptop.sh
+++ link_laptop.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/zsh
 
-ln -s dot-config/hypr/overrides/laptop.conf dot-config/hypr/overrides/overides.conf
-ln -s dot-config/ghostty/laptop_config dot-config/ghostty/config
-ln -s dot-config/fuzzel/laptop.ini dot-config/fuzzel/fuzzel.ini
+ln -s dot-config/hypr/overrides/laptop.conf ~/.config/hypr/overrides/overides.conf
+ln -s dot-config/ghostty/laptop_config ~/.config/ghostty/config
+ln -s dot-config/fuzzel/laptop.ini ~/.config/fuzzel/fuzzel.ini
--- link_pc.sh
+++ link_pc.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/zsh
 
-ln -s dot-config/hypr/overrides/pc.conf dot-config/hypr/overrides/overides.conf
-ln -s dot-config/ghostty/pc_config dot-config/ghostty/config
-ln -s dot-config/fuzzel/pc.ini dot-config/fuzzel/fuzzel.ini
+ln -s dot-config/hypr/overrides/pc.conf ~/.config/hypr/overrides/overides.conf
+ln -s dot-config/ghostty/pc_config ~/.config/ghostty/config
+ln -s dot-config/fuzzel/pc.ini ~/.config/fuzzel/fuzzel.ini
